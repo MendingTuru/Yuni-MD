a254912ddd1a76c9d9c69d2a7671072e614be24e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-11-13 02:02:20 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -1,17 +1,16 @@
-let fetch = require('node-fetch')
-let teks = 'gcbot'
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
-
+const fetch = require('node-fetch')
+let teks = 'gckoko'
+let handler = async (m, { conn }) => conn.send2ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/480d83b0ef52cd62d5c71.jpg' + teks)).buffer(), `
 *𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐉𝐚𝐧𝐠𝐚𝐧 𝐋𝐮𝐩𝐚 𝐉𝐨𝐢𝐧 && 𝐁𝐚𝐜𝐚 𝐑𝐮𝐥𝐞𝐬❗*
 ╔═══════════════════
-║ _*Group All*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
+║ _*Group All*_ ᵈʳˢYᴜɴɪ-ᴍᴅ×፝֟͜×
 ╠═══════════════════
-║➸ 𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 1
-║ https://tinyurl.com/2nchxnpa
-║➸ 𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 2
-║ https://tinyurl.com/2gptyhqs
-║➸ 𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 3
-║ https://bit.ly/3zvNS1w
+║➸ _Group Yuni & Alpuy:_
+║ https://chat.whatsapp.com/L4FEDmo5VS870hYYm22t7R
+║➸ _𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 1:_
+║ https://chat.whatsapp.com/IqFfoXcdh7UFyPoQTpKoQp
+║➸ _𝐆𝐫𝐨𝐮𝐩 𝐁𝐨𝐭 2:_
+║ https://chat.whatsapp.com/LhpFRMqsKkn6nrCCf1He9R
 ╚════════════════════
 ╔════════════════════
 ║ *RULES GROUP BOT*
@@ -25,10 +24,9 @@ let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fe
 ║𝐃𝐢𝐥𝐚𝐫𝐚𝐧𝐠 𝐇𝐢𝐧𝐚 𝐁𝐨𝐭
 ║𝐌𝐚𝐮 𝐒𝐞𝐰𝐚? 𝐊𝐞𝐭𝐢𝐤 .owner
 ╚════════════════════
-
-`.trim(), wm, 'Owner', '.owner', 'Menu', '.menu')
-handler.help = ['gcbot']
+`.trim(), wm, '😳KOKO', '.owner', 'MENU✔️', '.menu')
+handler.help = ['gckoko']
 handler.tags = ['info']
-handler.command = /^gc(bot|groupbot)$/i
+handler.command = /^gc(koko|groupkoko)$/i
 
 module.exports = handler
